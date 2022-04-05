9edf7b3bc91f1bc6f4a04143887cc508b63ec0d0
PULIMIAMULYA <pulimiamulya@gmail.com>
2022-04-05 16:40:53 +0530

    added content on about page

--- Frontend/src/home/about.js
+++ Frontend/src/home/about.js
@@ -11,9 +11,9 @@ function About () {
     <p className='text-center text-dark'>
     Institute of visvodaya society (VTA) invites you to the visvotshavam
 A very useful and entertaining program designed to exhibit various cultural events , technical events, sports events, funevents, literature events 
-Under the category of cultural events, we introduced  the compititions like dance, drama, singing, painting 
+Under the category of cultural events we introduced  the compititions like dance, drama, singing, painting 
 And the technical events are coding, debuging, poster presentation, robotics and the sports events are batminton, cricket, chess, volleyball and fun events are photography, short film and the literature events are creative writing, debate, poetry, Essay writing 
-It will be going to be a fabulous and remembering function
+It will be going to be a fabulous and remembering function.
     </p>
     <div className='container text-center'>
     <Link to='/register' style={{textDecoration :'none'}}><button type="button" className="btn btn-warning">Join With Us by Registering now!!</button></Link>
